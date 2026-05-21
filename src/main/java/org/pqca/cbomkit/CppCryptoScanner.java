8e7ae95326c31fe942e21de4eef260fe9d1a7546
Charan Sai <guttulacharansai@gmail.com>
2026-05-21 11:07:01 +0530

    feat: implement Eclipse CDT headless parser initialization

--- src/main/java/org/pqca/cbomkit/CppCryptoScanner.java
+++ src/main/java/org/pqca/cbomkit/CppCryptoScanner.java
@@ -1,5 +1,13 @@
 package org.pqca.cbomkit;
 
+import org.eclipse.cdt.core.parser.FileContent;
+import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
+import org.eclipse.cdt.core.parser.ScannerInfo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This engine utilizes Eclipse CDT to perform headless AST parsing
  * of C/C++ source files to detect cryptographic primitives.
@@ -10,8 +18,20 @@ public class CppCryptoScanner {
         System.out.println("Initializing C++ AST Scanner...");
         String targetFile = "test.cpp";
         
-        // TODO: Initialize Eclipse CDT headless parser components
+        if (!new File(targetFile).exists()) {
+            System.err.println("Error: Target file not found -> " + targetFile);
+            return;
+        }
         
-        System.out.println("Scanner execution completed.");
+        // 1. Initialize CDT Headless Parser components
+        FileContent fileContent = FileContent.createForExternalFileLocation(targetFile);
+        
+        Map<String, String> macroDefinitions = new HashMap<>();
+        String[] includeSearchPaths = new String[0]; 
+        
+        ScannerInfo scannerInfo = new ScannerInfo(macroDefinitions, includeSearchPaths);
+        IncludeFileContentProvider provider = IncludeFileContentProvider.getEmptyFilesProvider();
+
+        // TODO: Generate Translation Unit from the parser
     }
 }
